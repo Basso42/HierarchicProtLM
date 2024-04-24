05f28bac832cbc0abf56866f13a6ff700c8571cb
gamer35 <florian.basso@ensae.fr>
2024-04-24 19:34:01 +0000

    added backbone and began adding focal-loss, need to add weighted loss and build small testing pipeline with training loop.

---
+++ ankh/__init__.py
@@ -0,0 +1,10 @@
+from ankh.models.convbert_multilabel_classification import (
+    ConvBertForMultiLabelClassification,
+)
+
+from .ankh_transformers import (
+    get_available_models,
+    load_base_model,
+    load_large_model,
+    load_model,
+)
