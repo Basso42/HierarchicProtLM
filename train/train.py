8de8ff8c82e00b5e5837ed0f148ee58159ee8c0f
gamer35 <florian.basso@ensae.fr>
2024-05-08 23:12:51 +0000

    adding weights

--- train/train.py
+++ train/train.py
@@ -80,30 +80,6 @@ def create_dataset(tokenizer,seqs,labels, max_length):
 
 
 
-#Saving only fine-tuned weights
-class Save_LoRA_Callback(TrainerCallback):
-    """
-    Allows to save only finetuned weights inside the Trainer function during training. (10 mbs instead of 2 gigas for T5)
-    """
-    def __init__(self, path):
-        self.path = path
-
-    def on_epoch_end(self, args, state, control, model, **kwargs): #each time training part in epoch ends, this function is executed
-        if args.local_rank == 0:
-            file_list = os.listdir(os.path.dirname(self.path))
-            epochs = [int(re.search(r'\d+', filename).group()) for filename in file_list if re.search(r'\d+', filename)] #looking for last epoch
-        
-            if epochs:
-                last_epoch = max(epochs)
-            else: 
-                last_epoch = 0
-
-            #writing weights only for non frozen parameters
-            save_model(model, filepath=self.path.replace('.',f'_epoch_{last_epoch+1}.'))  
-        
-            print(f"Weights of {last_epoch+1}th epoch saved")
-
-
 # Main training fuction
 def train_per_protein(
         train_df,         #training data
@@ -142,7 +118,7 @@ def train_per_protein(
         if filtered_paths:
             latest_epoch_weights = max(filtered_paths, key=lambda path: int(path.split('_')[-1].split('.')[0]), default=None)
             print(f"Resuming training with the {latest_epoch_weights} weights.")
-            model, tokenizer = load_model(f"{path_weights}/{latest_epoch_weights}", num_labels=num_labels, mixed=True, loss_type=loss_type, loss_weights=loss_weights)
+            model, tokenizer = load_model(f"{path_weights}/{latest_epoch_weights}", num_labels=num_labels, loss_type=loss_type, loss_weights=loss_weights)
         else:
             model, tokenizer = PT5_classification_model(num_labels=num_labels, half_precision=True, loss_type=loss_type, loss_weights=loss_weights)
     else:
@@ -200,8 +176,8 @@ def train_per_protein(
         train_dataset=train_set,
         eval_dataset=valid_set,
         tokenizer=tokenizer,
-        compute_metrics=compute_metrics,
-        callbacks=[Save_LoRA_Callback(save_path)])    
+        compute_metrics=compute_metrics
+    )    
     
     print("On va commencer l'entrainement")
     # Train model
@@ -218,8 +194,8 @@ print(f"Working with {num_labels} classes using {loss_type} loss, weighted : {we
 
 #Relative paths using variables in the run.sh file
 data_path = sys.argv[2] + '/data/'
-os.system(f"mkdir -p {sys.argv[2]}/ProtTrans/LoRA_Checkpoints_{loss_type}")
-save_path = sys.argv[2] + f'/ProtTrans/LoRA_Checkpoints_{loss_type}/LoRA_finetuned.pth'
+os.system(f"mkdir -p {sys.argv[2]}/train/SingleTaskClassif_Checkpoints_{loss_type}")
+save_path = sys.argv[2] + f'/train/SingleTaskClassif_Checkpoints_{loss_type}/ankh_finetuned.pth'
 
 print(sys.argv[2], sys.argv[3])
 
@@ -244,4 +220,3 @@ print(f"Protein maximum length is {max_length}")
 tokenizer, model, history = train_per_protein(train_dataset, validation_dataset, num_labels=num_labels, loss_type=loss_type, loss_weights=weights, batch=batch_size, accum=8, epochs=5, seed=42, bool_deepspeed=True, gpu=num_gpus, max_length=max_length, save_path=save_path, load_last_finetuned_weights=True)
 
 print("Model trained")
-
